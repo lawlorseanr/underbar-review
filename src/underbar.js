268460d6fc7a30b11c452106f66550abab562891
Louis McGruder <maenkindinc@gmail.com>
2021-06-15 12:39:24 -0700

    Underbar - Part 1: last, first, each

--- src/underbar.js
+++ src/underbar.js
@@ -38,7 +38,16 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    if (n === undefined) {
+      return array[array.length - 1];
+    }
+
+    if (n > array.length - 1) {
+      return array;
+    }
+    return array.slice(array.length - n);
   };
+
 
   // Call iterator(value, key, collection) for each element of collection.
   // Accepts both arrays and objects.
